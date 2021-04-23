f52309cefba1e0fd1b5d838b233d2e605a65d3f3
Salman Asif <salmanasif35@gmail.com>
2021-04-23 22:57:48 +0530

    Update checkout.php

--- im/checkout.php
+++ im/checkout.php
@@ -9,7 +9,7 @@ $pvalue = $_SESSION['PHONE'];
 $orderId = $_POST["orderId"];
 $orderAmount = $_POST["amount"];
 
-$host = "https://stack23.herokuapp.com";
+$host = "https://www.ieeestack.com";
 $notifyUrl = $host. "/im/notify.php";
 $returnUrl = $host. "/im/return.php";
 
