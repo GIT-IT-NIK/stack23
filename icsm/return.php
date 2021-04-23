41bdd6c21a6818195b4446c1c25ce5e9534156d4
Salman Asif <salmanasif35@gmail.com>
2021-04-23 21:40:35 +0530

    Update return.php

--- icsm/return.php
+++ icsm/return.php
@@ -13,11 +13,23 @@ $data = $orderId.$orderAmount.$referenceId.$txStatus.$paymentMode.$txMsg.$txTime
 $secretKey = "f0051992cf1487372eaa81f964c071811e8108ed";
 $hash_hmac = hash_hmac('sha256', $data, $secretKey, true) ;
 $computedSignature = base64_encode($hash_hmac);
-if ($signature == $computedSignature) {
-   echo "<h1>Your order is successfully confirmed!</h1>";
- } else {
-   echo "<h1>Something went wrong</h1>";
-  // Reject this call
-}
+
+if($txStatus == "SUCCESS")
+{
+	if ($signature == $computedSignature){
+   		//echo "<h1>Your order is successfully confirmed!</h1>";
+   		header("refresh:1;url=https://stack23.herokuapp.com/icsm/Landing_Page.html");
+	}
+	else {
+   		//echo "<h1>Something went wrong</h1>";
+   		header("refresh:1;url=https://stack23.herokuapp.com/icsm/Failed.html");
+	} 
+ }
+ 
+else{
+	//echo $txStatus."</br>";
+	header("refresh:1;url=https://stack23.herokuapp.com/icsm/Failed.html");
+} 
+
 
  ?>
